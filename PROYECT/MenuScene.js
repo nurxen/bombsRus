7530662ae9689895a23bd6f8cb8f9a66e8c62782
nurxen <nurxen.21@gmail.com>
2024-11-16 19:34:19 +0100

    Import assets to scene

--- PROYECT/MenuScene.js
+++ PROYECT/MenuScene.js
@@ -5,12 +5,13 @@ class MenuScene extends Phaser.Scene {
 
     create() {
         // Agregar la imagen de fondo y ajustarla al tamaño del canvas
-        const bg = this.add.image(0, 0, 'bg')
+        const loseBackgroung = this.add.image(0, 0, 'LoseBackground')
             .setOrigin(0) // Establece el origen en la esquina superior izquierda
             .setDisplaySize(this.sys.game.config.width, this.sys.game.config.height); // Ajusta al tamaño del canvas
 
         // Crear el botón de "Start Game"
-        const startButton = this.add.image(640, 400, 'pato')
+        const startButton = this.add.image(640, 500, 'RetryButtonGrande')
+            .setScale(0.2)
             .setOrigin(0.5, 0.5)
             .setInteractive() // Hacer el botón interactivo
             .on('pointerdown', () => {
@@ -39,8 +40,8 @@ class MenuScene extends Phaser.Scene {
 
         // Opcional: Animaciones o efectos al pasar el ratón sobre los botones
         [startButton, exitButton].forEach(button => {
-            button.on('pointerover', () => button.setScale(1.1)); // Aumentar ligeramente el tamaño
-            button.on('pointerout', () => button.setScale(1)); // Volver al tamaño original
+            button.on('pointerover', () => button.setScale(0.21)); // Aumentar el tamaño
+            button.on('pointerout', () => button.setScale(0.2)); // Volver al tamaño original
         });
     }
 }
